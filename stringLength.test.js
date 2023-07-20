5e5af0a51baca8a51dc1c2d58bb7e6aa0016adcc
Benson Kiai <benson.kiai86@gmail.com>
2023-07-20 15:08:22 +0300

    Update test.js to reflect updated function checks

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,5 +1,8 @@
+const { describe } = require('yargs');
 const stringLength = require('./stringLength');
-
-test('stringLength returns the length of a string', () => {
-    expect(stringLength('Hello World!')).toBe(12);
-    });
+// test if stringLength is less than 1 or greater than 10 to throw error  
+test('stringLength', () => {
+    expect(() => stringLength('')).toThrow('The length of the string must be between 1 and 10 characters long.');
+    expect(() => stringLength('12345678901')).toThrow('The length of the string must be between 1 and 10 characters long.');
+    expect(() => stringLength('1234567890')).not.toThrow('The length of the string must be between 1 and 10 characters long.');
+});
