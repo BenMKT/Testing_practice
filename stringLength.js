25ea05a560793a4a8216196ca70f9cc6f0b76566
Benson Kiai <benson.kiai86@gmail.com>
2023-07-20 15:55:33 +0300

    Fix ESLint errors

--- stringLength.js
+++ stringLength.js
@@ -1,8 +1,6 @@
 function stringLength(string) {
   if (string.length < 1 || string.length > 10) {
-    throw new Error(
-      'The length of the string must be between 1 and 10 characters long.'
-    );
+    throw new Error('The length of the string must be between 1 and 10 characters long.');
   } else {
     return string.length;
   }
