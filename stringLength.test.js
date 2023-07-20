83041f8a33fcce7eeefe35a20fd86c35243f656f
Benson Kiai <benson.kiai86@gmail.com>
2023-07-20 15:41:23 +0300

    Fix ESlinter errors

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,7 +1,7 @@
 const stringLength = require('./stringLength.js');
-// test if stringLength is less than 1 or greater than 10 to throw error  
+// test if stringLength is less than 1 or greater than 10 to throw error
 test('stringLength', () => {
-    expect(() => stringLength('')).toThrow('The length of the string must be between 1 and 10 characters long.');
-    expect(() => stringLength('12345678901')).toThrow('The length of the string must be between 1 and 10 characters long.');
-    expect(() => stringLength('1234567890')).not.toThrow('The length of the string must be between 1 and 10 characters long.');
+  expect(() => stringLength('')).toThrow('The length of the string must be between 1 and 10 characters long.');
+  expect(() => stringLength('12345678901')).toThrow('The length of the string must be between 1 and 10 characters long.');
+  expect(() => stringLength('1234567890')).not.toThrow('The length of the string must be between 1 and 10 characters long.');
 });
